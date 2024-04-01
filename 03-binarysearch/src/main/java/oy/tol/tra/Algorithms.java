d9103bad9e97cbf6cda6a4472c73d76b3e58439b
yewtquyt <2647015002@qq.com>
2024-04-01 23:20:07 +0800

    EXERCISE

--- 03-binarysearch/src/main/java/oy/tol/tra/Algorithms.java
+++ 03-binarysearch/src/main/java/oy/tol/tra/Algorithms.java
@@ -1,6 +1,27 @@
 package oy.tol.tra;
 
 public class Algorithms {
+    public static <T extends Comparable<T>> void sort(T[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j].compareTo(array[j + 1]) > 0) {
+                    T temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static <T> void reverse(T[] array) {
+        int i = 0;
+        while (i < array.length / 2) {
+            T temp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = temp;
+            i++;
+        }
+    }
 
     public static <T extends Comparable<T>> int binarySearch(T aValue, T[] fromArray, int fromIndex, int toIndex) {
         if (fromIndex > toIndex) {
